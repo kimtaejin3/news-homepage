fbd7c9bba5b6e00cee64f3385210bfc9b00e37c5
taejin Kim <rlaxowls1316@naver.com>
2023-04-27 12:14:41 +0900

    feat: Add mobile nav

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const headings_toggle = document.querySelector(".headings-toggle");
+const mobile_nav = document.querySelector(".mobile_nav");
+const mobile_nav_toggle = document.querySelector(".mobile_nav-toggle");
+
+headings_toggle.addEventListener("click", () => {
+  mobile_nav.style.display = "initial";
+});
+
+mobile_nav_toggle.addEventListener("click", () => {
+  mobile_nav.style.display = "none";
+});

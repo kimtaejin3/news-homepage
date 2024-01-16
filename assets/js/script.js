3827161c4530b4df2d33a19803e66d22f50f7ae1
Taejin Kim <rlaxowls1316@naver.com>
2024-01-16 13:54:36 +0900

    토글메뉴 height 수정: body에 relative

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,19 @@ const headings_toggle = document.querySelector(".headings-toggle");
 const mobile_nav = document.querySelector(".mobile_nav");
 const mobile_nav_toggle = document.querySelector(".mobile_nav-toggle");
 
-headings_toggle.addEventListener("click", () => {
+headings_toggle.addEventListener("click", (e) => {
+  e.stopPropagation();
   mobile_nav.style.display = "initial";
 });
 
-mobile_nav_toggle.addEventListener("click", () => {
+mobile_nav_toggle.addEventListener("click", (e) => {
   mobile_nav.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target.classList.value !== "mobile_nav") {
+    mobile_nav.style.display = "none";
+  }
 });
 
 window.addEventListener("resize", () => {
